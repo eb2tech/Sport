f43729353e8cf0788433e87223b19d8d9f5f2740
Ed Blakes <eblakes@eprod.com>
2017-10-18 09:32:01 -0500

    Using eb2tech instance

--- Sport.Mobile.Shared/Keys.cs
+++ Sport.Mobile.Shared/Keys.cs
@@ -10,10 +10,10 @@ namespace Sport.Mobile.Shared
 		public static readonly string MobileCenterKeyiOS = string.Empty;
         public static readonly string MobileCenterKeyAndroid = string.Empty;
 
-        public static readonly string SourceCodeUrl = "https://github.com/xamarin/sport";
+        public static readonly string SourceCodeUrl = "https://github.com/eb2tech/sport";
 		public static readonly string GooglePushNotificationSenderId = "643221174110";
         public static readonly string AzureDomainLocal = "http://10.211.55.3/Sport.Service/";
-        public static readonly string AzureDomainRemote = "https://xamarin-sportv2.azurewebsites.net/";
+        public static readonly string AzureDomainRemote = "http://sportservicetest.azurewebsites.net";
 
 		public static readonly string GoogleClientIdiOS = "707751590419-1rv4eautkn4qlt8djvkbqa6nov0p7f0p.apps.googleusercontent.com"; //iOS App
 		public static readonly string GoogleServerIdiOS = "707751590419-6kh953g6p97ad1b7iolcoar5liepacnd.apps.googleusercontent.com"; //WebApp for server
